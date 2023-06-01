adff307236c18b30386aeb78dda30c10e86970b5
qiruiw <bitwqr@gmail.com>
2023-06-01 01:37:17 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ conda install -c pytorch pytorch=1.7.0 torchvision cudatoolkit=11.0
 conda install -c fvcore -c iopath -c conda-forge fvcore iopath
 conda install -c bottler nvidiacub
 # NOTE: Tested with 0.5 and 0.6
-conda install pytorch3d -c pytorch3d-nightly
+pip install "git+https://github.com/facebookresearch/pytorch3d.git@v0.6.0" 
 
 # detectron2
 python -m pip install detectron2==0.3 -f \
